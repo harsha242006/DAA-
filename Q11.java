e784855b6ca7fc1c8bb4b7c0b97368e1fa03fade
harsha242006 <harshamoorthy6336@gmail.com>
2025-05-18 19:27:58 +0530

    Update Q11.java

--- Q11.java
+++ Q11.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
-public class Swap {
+public class BubbleSort {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt();
-        int temp = a; a = b; b = temp;
-        System.out.println("After Swap: a=" + a + ", b=" + b);
+        int[] arr = {5, 1, 4, 2, 8};
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp;
+                }
+        for (int x : arr) System.out.print(x + " ");
     }
 }
