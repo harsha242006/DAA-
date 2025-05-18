b7add7d701995f539f11e1023908a54b174448c8
harsha242006 <harshamoorthy6336@gmail.com>
2025-05-18 19:54:53 +0530

    Update Q13.java

--- Q13.java
+++ Q13.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class AnagramChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,7 +12,6 @@ public class AnagramChecker {
             System.out.println("Not Anagrams");
             return;
         }
-
         char[] a1 = str1.toCharArray();
         char[] a2 = str2.toCharArray();
         Arrays.sort(a1);
