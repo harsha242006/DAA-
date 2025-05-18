07fdf56bb9dd5c0ef414674518f4cc25486829c5
harsha242006 <harshamoorthy6336@gmail.com>
2025-05-18 19:30:23 +0530

    Create Q13.java

---
+++ Q13.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().replaceAll("\\s", "").toLowerCase();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Anagrams");
+            return;
+        }
+
+        char[] a1 = str1.toCharArray();
+        char[] a2 = str2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        if (Arrays.equals(a1, a2)) {
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
+        }
+    }
+}
