36ca2bd915db48571c95d2ee83bafed1bc9a84b0
harsha242006 <harshamoorthy6336@gmail.com>
2025-05-18 19:44:34 +0530

    Create Q18.java

---
+++ Q18.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class PairSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, -1, 5};
+        int sum = 6;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            int complement = sum - num;
+            if (map.getOrDefault(complement, 0) > 0)
+                System.out.println("(" + complement + ", " + num + ")");
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+    }
+}
